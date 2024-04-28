2c71cb40a32de34818db4ec3b20b7ff378890b38
Rup-Royofficial <rupayanroy981@gmail.com>
2024-04-29 00:39:32 +0530

    inclusion of  comparisons 04

---
+++ 04_Comparisons.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+//The above form of comparisons are applicable and appreciated, why because, it is basically comparisons between two similar datatypes
+
+
+//The below form of comparisons are not appropriate and are not appreciated, as they mean comparison between two different datatypes
+//Try to avoid below form of comparisons most of the times
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
