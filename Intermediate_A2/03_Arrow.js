ad1f48d5f799229d1ceba0e8a8ac0a55f92c72cd
Rup-Royofficial <rupayanroy981@gmail.com>
2024-05-11 22:47:34 +0530

    day12 : works done in JS arrow funcs

--- Intermediate_A2/03_Arrow.js
+++ Intermediate_A2/03_Arrow.js
@@ -1,1 +1,55 @@
 // Arrow functions in JS
+const user = {
+    username: "abc",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "abc"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "abc"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "abc"
+    console.log(this);
+}
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "abc"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
