e27beb83fd75f6d67af362df0e6dd86b61ce5c51
Rup-Royofficial <rupayanroy981@gmail.com>
2024-05-26 19:03:15 +0530

    day23 : async,await, fetch()  ... :|

--- Advanced_A2/APIs/Promises.js
+++ Advanced_A2/APIs/Promises.js
@@ -42,16 +42,18 @@ const promiseFour = new Promise(function(resolve, reject){
     }, 1000)
 })
 
+
+//concise form
  promiseFour
  .then((user) => {
     console.log(user);
     return user.username
 }).then((username) => {
     console.log(username);
-}).catch(function(error){
+}).catch(function(error){ // for errors
     console.log(error);
 }).finally(() => console.log("The promise is either resolved or rejected"))
-
+//finally() end format , depends on u , if u want to use
 
 
 const promiseFive = new Promise(function(resolve, reject){
@@ -65,7 +67,11 @@ const promiseFive = new Promise(function(resolve, reject){
     }, 1000)
 });
 
+
+
+//async & await
 async function consumePromiseFive(){
+    // necessity of try&catch is due to errors
     try {
         const response = await promiseFive
         console.log(response);
@@ -76,27 +82,42 @@ async function consumePromiseFive(){
 
 consumePromiseFive()
 
-// async function getAllUsers(){
-//     try {
-//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+async function getAllUsers(){
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
 
-//         const data = await response.json()
-//         console.log(data);
-//     } catch (error) {
-//         console.log("E: ", error);
-//     }
-// }
+        const data = await response.json()
+        console.log(data);
+    } catch (error) {
+        console.log("E: ", error);
+    }
+}
 
-//getAllUsers()
+getAllUsers()
+//This would initially yield a Promise(), awaiting , where the promiseState is fulfilled but result is undefined
+/*
+Then it yields the results, after a some time
 
+0 :  {id: 1, name: 'Leanne Graham', username: 'Bret', email: 'Sincere@april.biz', address: {…}, …}
+1 :  {id: 2, name: 'Ervin Howell', username: 'Antonette', email: 'Shanna@melissa.tv', address: {…}, …}
+2 :  {id: 3, name: 'Clementine Bauch', username: 'Samantha', email: 'Nathan@yesenia.net', address: {…}, …}
+3 :  {id: 4, name: 'Patricia Lebsack', username: 'Karianne', email: 'Julianne.OConner@kory.org', address: {…}, …}
+4 :  {id: 5, name: 'Chelsey Dietrich', username: 'Kamren', email: 'Lucio_Hettinger@annie.ca', address: {…}, …}
+5 :  {id: 6, name: 'Mrs. Dennis Schulist', username: 'Leopoldo_Corkery', email: 'Karley_Dach@jasper.info', address: {…}, …}
+6 :  {id: 7, name: 'Kurtis Weissnat', username: 'Elwyn.Skiles', email: 'Telly.Hoeger@billy.biz', address: {…}, …}
+7 :  {id: 8, name: 'Nicholas Runolfsdottir V', username: 'Maxime_Nienow', email: 'Sherwood@rosamond.me', address: {…}, …}
+8 :  {id: 9, name: 'Glenna Reichert', username: 'Delphine', email: 'Chaim_McDermott@dana.io', address: {…}, …}
+9 :  {id: 10, name: 'Clementina DuBuque', username: 'Moriah.Stanton', email: 'Rey.Padberg@karina.biz', address: {…}, …}
+length: 10
+*/
+
+
+//General form of calling & using apis
 fetch('https://api.github.com/users/Rup-Royofficial')
 .then((response) => {
-    return response.json()
+    return response.json() //converting to json
 })
 .then((data) => {
     console.log(data);
 })
 .catch((error) => console.log(error))
-
-// promise.all
-// yes this is also available, kuch reading aap b kro.
