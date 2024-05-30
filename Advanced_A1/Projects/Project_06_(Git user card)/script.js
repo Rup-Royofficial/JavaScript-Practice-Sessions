d8aecb5b4353f9336dd47eb6d45db754a1540c87
Rup-Royofficial <rupayanroy981@gmail.com>
2024-05-30 17:32:36 +0530

    day25: working with APIs #3...

--- Advanced_A1/Projects/Project_06_(Git user card)/script.js
+++ Advanced_A1/Projects/Project_06_(Git user card)/script.js
@@ -1,15 +1,38 @@
 const inputData = document.querySelector('.github-username')
 const submitBtn = document.querySelector('.submit-data')
+const cardContainer = document.querySelector('.display-card')
 
-async function getData() {
-    const response = await fetch("https://api.github.com/users/rup-royofficial");
+async function getData(username) {
+    const response = await fetch(`https://api.github.com/users/${username}`);
     const data = await response.json();
 
     return data;
 }
 
+// function lowerCase(str) {
+//     return str.toLowerCase();
+// }
 
-submitBtn.addEventListener('click', async function(e) {
-    const result = await getData();
+/**
+ * @argument name, public_repos, followers, following, company
+ */
+
+submitBtn.addEventListener('click', async function() {
+    
+    const inputValue  = inputData.value.toLowerCase();
+    const result = await getData(inputValue);
     console.log(result);
+    const userCompany = (result.company==null) ? "Unemployed" : `${result.company}`
+    cardContainer.innerHTML = `
+        <div class="card">
+            <img src="${result.avatar_url}" alt="github-img" id="user-avatar"/>
+            <div class="text-data">
+                <p><span class="card-info">Name</span>: ${result.name}</p>
+                <p><span class="card-info">No. of repos(public)</span>: ${result.public_repos}</p>
+                <p><span class="card-info">Followers</span>: ${result.followers}</p>
+                <p><span class="card-info">Following</span>: ${result.following}</p>
+                <p><span class="card-info">Company</span>: ${userCompany}</p>
+            </div>
+        </div>
+    `
 })
