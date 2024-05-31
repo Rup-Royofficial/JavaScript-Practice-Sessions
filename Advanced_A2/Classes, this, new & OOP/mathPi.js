8f543e91515d7183989410b495f2c805ec5181e0
Rup-Royofficial <rupayanroy981@gmail.com>
2024-06-01 00:50:11 +0530

    day26: working on defineProperty#2 ...

--- Advanced_A2/Classes, this, new & OOP/mathPi.js
+++ Advanced_A2/Classes, this, new & OOP/mathPi.js
@@ -6,27 +6,36 @@ const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
 // Math.PI = 5
 // console.log(Math.PI);
 
-const chai = {
-    name: 'ginger chai',
+const one = {
+    name: 'ginger',
     price: 250,
     isAvailable: true,
 
     orderChai: function(){
-        console.log("chai nhi bni");
+        console.log("Problem faced");
     }
 }
 
-console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+console.log(Object.getOwnPropertyDescriptor(one, "name"));
+/*
+value: 'ginger',
+configurable: true,
+enumerable: true,
+writable: true
+*/
 
-Object.defineProperty(chai, 'name', {
+
+
+Object.defineProperty(one, 'name', {
     //writable: false,
-    enumerable: true,
+    enumerable: true, // if false, then as shown in the above comment, the enumerable property will change to false
+    // if turned to false then the below enumerable property(for loop) will not work out for the name key 
     
 })
 
-console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+console.log(Object.getOwnPropertyDescriptor(one, "name"));
 
-for (let [key, value] of Object.entries(chai)) {
+for (let [key, value] of Object.entries(one)) {
     if (typeof value !== 'function') {
         
         console.log(`${key} : ${value}`);
